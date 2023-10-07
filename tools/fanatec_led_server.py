f3f78ebf209e8ae6ec327e10a35cc6c3546f76df
wegreenall <wjgreenall@gmail.com>
2023-10-07 11:34:01 +0300

    Add further formatting changes due to use of Black formatter

--- tools/fanatec_led_server.py
+++ tools/fanatec_led_server.py
@@ -251,4 +251,3 @@ if __name__ == "__main__":
         ev.set()
         if args.dbus:
             clear()
-        
--- tools/hid-fanatecff-test.py
+++ tools/hid-fanatecff-test.py
@@ -2,32 +2,43 @@ import sys
 import usb1
 import time
 
-VENDOR_ID = 0x0eb7
+VENDOR_ID = 0x0EB7
 DEVICE_ID = 0x0005
 
 CMD_BASE = [0x01]
-CMD_LED_BASE = [0xf8]
-CMD_BASE_LED = [0x13] # CMD_BASE+CMD_LED_BASE+CMD_BASE_LED+[val]
-CMD_WHEEL_LED = [0x09,0x08,0x00] # CMD_BASE+CMD_LED_BASE+CMD_WHEEL_LED+[val]
+CMD_LED_BASE = [0xF8]
+CMD_BASE_LED = [0x13]  # CMD_BASE+CMD_LED_BASE+CMD_BASE_LED+[val]
+CMD_WHEEL_LED = [0x09, 0x08, 0x00]  # CMD_BASE+CMD_LED_BASE+CMD_WHEEL_LED+[val]
 
-CMD_SIMPLE_SPRING_ENABLE = [0x11,0x0b]
-CMD_SIMPLE_SPRING_DISABLE = [0x13,0x0b]
-CMD_SIMPLE_SPRING_ARGS = [0x80,0x80,0x10,0x00,0x00] # offset?, offset?, coefficient, ??, saturation
+CMD_SIMPLE_SPRING_ENABLE = [0x11, 0x0B]
+CMD_SIMPLE_SPRING_DISABLE = [0x13, 0x0B]
+CMD_SIMPLE_SPRING_ARGS = [
+    0x80,
+    0x80,
+    0x10,
+    0x00,
+    0x00,
+]  # offset?, offset?, coefficient, ??, saturation
 
-CMD_FORCE_SPRING = [0x01,0x08] # CMD_BASE+CMD_FORCE_SPRING+[val] # val 0x80 -> no force; val 0x00 -> full force to the left
-CMD_FORCE_SPRING_DISABLE = [0x03,0x08]
+CMD_FORCE_SPRING = [
+    0x01,
+    0x08,
+]  # CMD_BASE+CMD_FORCE_SPRING+[val] # val 0x80 -> no force; val 0x00 -> full force to the left
+CMD_FORCE_SPRING_DISABLE = [0x03, 0x08]
+
 
 def payload(args):
-    p = bytearray(CMD_BASE+args)
+    p = bytearray(CMD_BASE + args)
     # p.extend(args)
     # pad with zeros
-    p.extend([0]*(8-len(p)))
+    p.extend([0] * (8 - len(p)))
     print(p)
     return p
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     val = None
-    if len(sys.argv)==2:
+    if len(sys.argv) == 2:
         val = int(sys.argv[1])
 
     with usb1.USBContext() as context:
@@ -37,13 +48,13 @@ if __name__ == '__main__':
             skip_on_error=True,
         )
         if handle is None:
-            print('Device not found')
+            print("Device not found")
             sys.exit(1)
 
         handle.setAutoDetachKernelDriver(True)
         with handle.claimInterface(0):
             # request reading of current value
-            handle.interruptWrite(0x01, payload(CMD_LED_BASE+CMD_WHEEL_LED+[val]))
+            handle.interruptWrite(0x01, payload(CMD_LED_BASE + CMD_WHEEL_LED + [val]))
             # handle.interruptWrite(0x01, payload(CMD_FORCE_SPRING+[val]))
             # time.sleep(1)
             # handle.interruptWrite(0x01, payload(CMD_FORCE_SPRING_DISABLE))
@@ -87,7 +98,7 @@ if __name__ == '__main__':
 # FFB test
 # Endpoint: 0x01, Direction: OUT
 # URB transfer type: URB_INTERRUPT (0x01)
-# Leftover Capture Data: 0000000000000000 # start 
+# Leftover Capture Data: 0000000000000000 # start
 # Leftover Capture Data: 0100000000000000
 # Leftover Capture Data: 0103080000000000
 # Leftover Capture Data: 01030b0000000000
@@ -203,7 +214,6 @@ if __name__ == '__main__':
 # Leftover Capture Data: 01130b0000000000
 
 
-
 # Spring Force: Force Spring -> spring effect
 # offset x=0 y=0
 # saturation x=10000 y=10000
@@ -255,7 +265,6 @@ if __name__ == '__main__':
 # Leftover Capture Data: 010108ea00000000
 
 
-
 # Spring Force: Amplitude Sweep -> constant effect ?
 # offset x=768 y=-768
 # saturation x=10000 y=10000
@@ -269,8 +278,6 @@ if __name__ == '__main__':
 # Leftover Capture Data: 010108a600000000
 # Leftover Capture Data: 010108a100000000
 # Leftover Capture Data: 0101089b00000000
-
-
 
 
 # Wheel angle/sensivity 801
@@ -305,6 +312,3 @@ if __name__ == '__main__':
 # Leftover Capture Data: 01f8813804000000
 # URB_INTERRUPT in
 # Leftover Capture Data: 0100000000000000
-
-
-
