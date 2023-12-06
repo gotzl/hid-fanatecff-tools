8f95b174534e98f382e323fb488d682d76b30e5a
Matthias Gorzellik <matthias.gorzellik@gmail.com>
2023-12-06 20:42:17 +0100

    look-up player index using the Rf2Scor struct #3

--- tools/rf2.py
+++ tools/rf2.py
@@ -7,15 +7,27 @@ from pyRfactor2SharedMemory import rF2data
 
 
 class RF2Client(fanatec_led_server.Client):
-    def __init__(self, ev, dbus=True, device=None, display='gear'): 
+    def __init__(self, ev, dbus=True, device=None, display='gear'):
         fanatec_led_server.Client.__init__(self, ev, dbus, device, display)
+        self._scoring = None
+        self._player_index = None
         self.telemetry = None
         self.mmap_check_cnt = 0
+
+    def _get_player_index(self):
+        for idx, vehicle in enumerate(self._scoring.mVehicles):
+            if vehicle.mIsPlayer:
+                return idx
+        return None
 
     def prerun(self):
         while not self.ev.isSet():
             try:
-                self.telemetry = rF2data.SimInfo().Rf2Tele.mVehicles[0]
+                sim_info = rF2data.SimInfo()
+                self._scoring = sim_info.Rf2Scor
+                self._player_index = self._get_player_index()
+                # get telemetry for player
+                self.telemetry = sim_info.Rf2Tele.mVehicles[self._player_index]
                 break
             except FileNotFoundError as e:
                 pass
@@ -24,12 +36,18 @@ class RF2Client(fanatec_led_server.Client):
     def tick(self):
         if not super().tick():
             return False
-            
-        # every once in a while check if shared files are still availabl
+
+        # every once in a while check if shared files are still available
+        # and player index is still the same
         if self.mmap_check_cnt > 10:
             self.mmap_check_cnt = 0
             try:
-                rF2data.SimInfo()
+                sim_info = rF2data.SimInfo()
+                # check player index
+                if not self._scoring.mVehicles[self._player_index].mIsPlayer:
+                    # index changed, look up the new index
+                    self._player_index = self._get_player_index()
+                    self.telemetry = sim_info.Rf2Tele.mVehicles[self._player_index]
             except FileNotFoundError as e:
                 return False
         self.mmap_check_cnt += 1
@@ -46,7 +64,7 @@ class RF2Client(fanatec_led_server.Client):
     @property
     def absInAction(self):
         return self.telemetry.mFilteredBrake != self.telemetry.mUnfilteredBrake
-        
+
     @property
     def speedKmh(self):
         return 3.6 * math.sqrt(self.telemetry.mLocalVel.x**2 + self.telemetry.mLocalVel.y**2 + self.telemetry.mLocalVel.z**2)
