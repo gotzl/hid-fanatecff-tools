fd35342429f58d0469b0ea61e27899ec62dafe3b
Matthias Gorzellik <matthias.gorzellik@gmail.com>
2023-12-12 21:56:32 +0100

    some experimental work on ui: * support non-dbus mode, default to CSLDD * changes for tuning-menu entries matching driver `dd_tuning_menu` branch * update UI when tuning-menu entries got changed on the wheel * allow file-drop of .pws (fanalab) files

--- tools/ui.py
+++ tools/ui.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-
-from pydbus import SystemBus
+import os
+import sys
+import time
+import pyudev
+from pyudev.pyqt5 import MonitorObserver
+from typing import List
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout,\
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, \
     QGroupBox, QComboBox, QLabel, QSlider, QPushButton
 
-bus = SystemBus()
-pdl = bus.get('org.fanatec.CSLElite', '/org/fanatec/CSLElite/Pedals')
-whl = bus.get('org.fanatec.CSLElite', '/org/fanatec/CSLElite/Wheel')
-base = bus.get('org.fanatec.CSLElite', '/org/fanatec/CSLElite')
+USE_DBUS = False
+
+if USE_DBUS:
+    from pydbus import SystemBus
+    bus = SystemBus()
+    pdl = bus.get('org.fanatec.CSLElite', '/org/fanatec/CSLElite/Pedals')
+    whl = bus.get('org.fanatec.CSLElite', '/org/fanatec/CSLElite/Wheel')
+    base = bus.get('org.fanatec.CSLElite', '/org/fanatec/CSLElite')
+else:
+    import fanatec_input
+    pdl = fanatec_input.CSLElitePedals()
+    whl = fanatec_input.CSLEliteWheel()
+    base = fanatec_input.CSLDD()
 
 app = QApplication([])
 
@@ -16,48 +29,57 @@ generalBoxLayout = QHBoxLayout()
 generalBox = QGroupBox("General")
 generalBox.setLayout(generalBoxLayout)
 
-btns = []
+
+btns: List[QPushButton] = []
+
+
 def updateRPM():
     whl.RPM = [b.isChecked() for b in btns]
 
+
 for i in range(9):
-    btn = QPushButton('RPM%i'%i)
+    btn = QPushButton('RPM%i' % i)
     btn.setCheckable(True)
     btn.clicked[bool].connect(updateRPM)
     generalBoxLayout.addWidget(btn)
     btns.append(btn)
 
-
 tuningBox = QGroupBox("Tuning Menu")
 
 slot = QComboBox()
-slot.addItems(['1','2','3','4','5'])
+slot.addItems(['None', 'Auto', '1', '2', '3', '4', '5'])
 slotLabel = QLabel("&Slot:")
 slotLabel.setBuddy(slot)
 
-TUNING_ENTRIES = ['SEN', 'FF', 'DRI', 'FEI', 'FOR', 'SPR', 'DPR', 'BLI', 'SHO']
+# FIXME: can these be exported from the driver?
 TUNING_ENTRIES_RANGE = {
-    'SEN': (9,108),
-    'FF': (0,100),
+    'SEN': (90, 2530),
+    'FF': (0, 100),
+    'FEI': (0, 100),
+    'FOR': (0, 120),
+    'SPR': (0, 120),
+    'DPR': (0, 120),
+
     'DRI': (-5, 0),
-    'FEI': (0,100),
-    'FOR': (0, 10),
-    'SPR': (0, 10),
-    'DPR': (0, 10),
     'BLI': (0, 100),
     'SHO': (0, 10),
+
+    'INT': (0, 20),
+    'NIN': (0, 100),
+    'FUL': (0, 100),
 }
+
 
 class TuningSlider(QWidget):
     def __init__(self, key, parent=None):
         super(TuningSlider, self).__init__(parent)
         self.key = key
         self.slider = QSlider(Qt.Horizontal, tuningBox)
-        self.slider.setMinimum(TUNING_ENTRIES_RANGE[i][0])
-        self.slider.setMaximum(TUNING_ENTRIES_RANGE[i][1])
+        self.slider.setMinimum(TUNING_ENTRIES_RANGE[key][0])
+        self.slider.setMaximum(TUNING_ENTRIES_RANGE[key][1])
         self.slider.valueChanged[int].connect(self.valuechange)
 
-        self.label = QLabel("&%s:"%key)
+        self.label = QLabel("&%s:" % key)
         self.label.setBuddy(self.slider)
 
         self.value = QLabel()
@@ -69,12 +91,14 @@ class TuningSlider(QWidget):
         self.setLayout(layout)
 
     def valuechange(self, x, updateSlider=False):
-        if updateSlider: 
+        if updateSlider:
             self.slider.blockSignals(True)
             self.slider.setValue(x)
             self.slider.blockSignals(False)
-        else: setattr(base, self.key, x)
+        else:
+            setattr(base, self.key, x)
         self.value.setText(str(x))
+
 
 tuningBoxLayout = QVBoxLayout()
 tuningSliderBoxLayout = QVBoxLayout()
@@ -82,19 +106,29 @@ tuningSliderBox = QGroupBox()
 tuningSliderBox.setLayout(tuningSliderBoxLayout)
 
 tuning_sliders = {}
-for i in TUNING_ENTRIES:
-    tuning_sliders[i] = TuningSlider(i)
-    tuningSliderBoxLayout.addWidget(tuning_sliders[i])
+for a in TUNING_ENTRIES_RANGE:
+    # only add the attributes the current base offers
+    if not hasattr(base, a):
+        continue
+    tuning_sliders[a] = TuningSlider(a)
+    tuningSliderBoxLayout.addWidget(tuning_sliders[a])
 
 tuningBoxLayout.addWidget(slotLabel)
 tuningBoxLayout.addWidget(slot)
 tuningBoxLayout.addWidget(tuningSliderBox)
 tuningBox.setLayout(tuningBoxLayout)
 
+
 def changeSlot(slot):
-    base.SLOT = int(slot)
+    if slot == 'None' or slot == 'Auto':
+        base.SLOT = 1
+    else:
+        base.SLOT = int(slot)
+    # wait for the slot change
+    time.sleep(0.2)
     for i, v in tuning_sliders.items():
         v.valuechange(getattr(base, i), updateSlider=True)
+
 
 slot.activated[str].connect(changeSlot)
 
@@ -102,10 +136,65 @@ layout = QVBoxLayout()
 layout.addWidget(generalBox)
 layout.addWidget(tuningBox)
 
-win = QWidget()
-win.setLayout(layout)
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("FanatecUI")
+        self.setLayout(layout)
+        self.setAcceptDrops(True)
+
+        context = pyudev.Context()
+        monitor = pyudev.Monitor.from_netlink(context)
+        monitor.filter_by(subsystem='hid')
+        self.observer = MonitorObserver(monitor)
+        self.observer.deviceEvent.connect(self.device_connected)
+        monitor.start()
+
+    def device_connected(self, device):
+        if 'DRIVER' not in device or device.get('DRIVER') != 'ftec_csl_elite':
+            return
+        # FIXME: should better check device.get('DEVPATH')
+        _pid = device.sys_name.split(':')[2].split('.')[0]
+        if _pid != base.device:
+            return
+        changeSlot(base.SLOT)
+
+    def dragEnterEvent(self, event):
+        mime_data = event.mimeData()
+        if mime_data.hasUrls() or 'XdndFileRoller0' in mime_data.formats():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        _path = None
+        mime_data = event.mimeData()
+        if mime_data.hasUrls():
+            _path = mime_data.text()
+        elif 'XdndFileRoller0' in mime_data.formats():
+            _path = mime_data.data('XdndFileRoller0')
+        if not os.path.isfile(_path):
+            print("Not a file: %s" % _path)
+            return
+        import xml.etree.ElementTree as ET
+        root = ET.parse(_path).getroot()
+        tuning_menu = root.find('TuningMenu')
+        if not tuning_menu:
+            return
+        # select the auto slot
+        base.SLOT = 1
+        slot.setCurrentIndex(1)
+        for i in tuning_menu:
+            if hasattr(base, i.tag):
+                tuning_sliders[i.tag].valuechange(i.text)
+
+
+win = MainWidget()
 win.show()
 
-changeSlot('1')
+# select current slot and fill values
+slot.setCurrentIndex(base.SLOT)
+changeSlot(base.SLOT)
 
-app.exec_()
+sys.exit(app.exec_())
